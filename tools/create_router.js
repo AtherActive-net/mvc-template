985a423935e83878134bbec32077d3feb6d4ed70
AtherActive <ikbensalty@gmail.com>
2022-09-05 09:33:11 +0200

    Init

---
+++ tools/create_router.js
@@ -0,0 +1,41 @@
+import fs from 'fs';
+
+const routerPath = `/src/routers/`
+
+console.log(process.argv0)
+function createNewRouter(name, mount) {
+    let routerFileString = `
+import express from 'express';
+
+// IMPORT YOUR CONTROLLER HERE!
+
+// ============================
+
+export const mount = '${mount}';
+export const router = express.Router();
+
+// Here you can create routes.
+
+const Router = {
+    mount: mount,
+    router: router
+}
+export default Router
+`
+
+    fs.open(process.cwd() + routerPath + (name + '.router.js'), 'w', (err, fd) => {
+        if(err) {
+            throw err
+        }
+
+        fs.writeFile(fd, routerFileString, (err) => {
+            if(err) {
+                throw err
+            }
+
+        })
+    })
+}
+
+
+createNewRouter(process.argv[2], process.argv[3])
