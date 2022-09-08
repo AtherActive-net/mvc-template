5a178094d2d5ec3e2b9eaee8308ddfc096445e29
AtherActive <ikbensalty@gmail.com>
2022-09-08 11:22:27 +0200

    feat: Added a create_model Script.

---
+++ tools/create_model.js
@@ -0,0 +1,34 @@
+import fs from 'fs';
+
+const modelPath = `/src/models/`
+
+
+function createNewModel(name) {
+    const modelString  =`
+import Sequelize from 'sequelize';
+import sequelize from '../database.js'
+
+// Here we define the model.
+const ${name} = sequelize.define('${name}', {
+
+})
+
+// All relations MUST be done in './relations.js'!
+
+export default ${name}
+`
+    fs.open(process.cwd() + modelPath + (name + '.model.js'), 'w', (err, fd) => {
+        if(err) {
+            throw err
+        }
+
+        fs.writeFile(fd, modelString, (err) => {
+            if(err) {
+                throw err
+            }
+
+        })
+    })
+}
+
+createNewModel(process.argv[2])
